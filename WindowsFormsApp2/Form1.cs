813927b8b6a35e5bb17e578ce6dd3e7d5f07cf51
chirchib <dmitry.kurochkin66@gmail.com>
2021-12-29 21:11:17 +0300

    Add project files.

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int[] set;
+
+        private void create_set1()
+        {
+            string[] string_set = textBox1.Text.Split(',');
+            set = new int[string_set.Length];
+            for (int i = 0; i < set.Length; ++i)
+                set[i] = Int32.Parse(string_set[i]);
+        }
+        // удаление эл-та
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int index;
+            try
+            {
+                create_set1();
+                index = Convert.ToInt32(textBox3.Text);
+                textBox1.Text = Set.Display(Set.removeElement(set, index));
+                textBox6.Text = Set.Display(Set.removeElement(set, index));
+            }
+            catch (InvalidCastException)
+            {
+                textBox6.Text = "Недопустимое значение";
+            }
+            catch (NullReferenceException)
+            {
+                textBox6.Text = "Поле пустое";
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                textBox6.Text = "Введите правильный индекс";
+            }
+            catch (Exception)
+            {
+                textBox6.Text = "Ошибка";
+            }
+        }
+
+        // добавление эт-та
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int num;
+            int index;
+            try
+            {
+                create_set1();
+                num = Int32.Parse(textBox4.Text);
+                index = Int32.Parse(textBox3.Text);
+                set = Set.addElement(set, index, num);
+                textBox1.Text = Set.Display(set);
+                textBox6.Text = Set.Display(set);
+            }
+            catch (InvalidCastException)
+            {
+                textBox6.Text = "Недопустимое значение";
+            }
+            catch (NullReferenceException)
+            {
+                textBox6.Text = "Поле пустое";
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                textBox6.Text = "Введите правильный индекс";
+            }
+            catch (Exception)
+            {
+                textBox6.Text = "Ошибка";
+            }
+        }
+
+        private int[] set1;
+
+        private void create_set2()
+        {
+            string[] string_set = textBox2.Text.Split(',');
+            set1 = new int[string_set.Length];
+            for (int i = 0; i < set1.Length; ++i)
+                set1[i] = Int32.Parse(string_set[i]);
+        }
+        // объединиьь
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                create_set1();
+                create_set2();
+                set = Set.mergeArrays(set, set1);
+                textBox1.Text = Set.Display(set);
+                textBox6.Text = Set.Display(set);
+            }
+            catch (InvalidCastException)
+            {
+                textBox6.Text = "Недопустимое значение";
+            }
+            catch (NullReferenceException)
+            {
+                textBox6.Text = "Поле пустое";
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                textBox6.Text = "Введите правильный индекс";
+            }
+            catch (Exception)
+            {
+                textBox6.Text = "Ошибка";
+            }
+        }
+
+        // опринадлежит
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int num;
+            try
+            {
+                create_set1();
+                num = Int32.Parse(textBox5.Text);
+                if (Set.isInArray(set, num))
+                {
+                    textBox6.Text = "Принадлежит";
+                }
+                else
+                {
+                    textBox6.Text = "Не принадлежит";
+                }
+            }
+            catch (InvalidCastException)
+            {
+                textBox6.Text = "Недопустимое значение";
+            }
+            catch (NullReferenceException)
+            {
+                textBox6.Text = "Поле пустое";
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                textBox6.Text = "Введите правильный индекс";
+            }
+            catch (Exception)
+            {
+                textBox6.Text = "Ошибка";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsApp2/Set.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    static class Set
+    {
+        
+        public static bool isInArray(int[] array, int element)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == element)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static int[] removeElement(int[] array, int index)
+        {
+            int[] result = new int[array.Length - 1];
+            int k = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (index != i)
+                {
+                    result[k] = array[i];
+                    k++;
+                }
+            }
+            return result;
+        }
+
+        public static int[] addElement(int[] array, int index, int element)
+        {
+            int[] result = new int[array.Length + 1];
+            for (int i = 0; i < index; i++)
+            {
+                result[i] = array[i];
+            }
+            result[index] = element;
+            for (int i = index + 1; i < result.Length; i++)
+            {
+                result[i] = array[i - 1];
+            }
+            return result;
+        }
+
+        public static int[] mergeArrays(int[] array1, int[] array2)
+        {
+            int[] result = new int[array1.Length + array2.Length];
+            for (int i = 0; i < array1.Length; i++)
+            {
+                result[i] = array1[i];
+            }
+            for (int i = array1.Length; i < result.Length; i++)
+                for (int j = 0; j < array2.Length; j++)
+                {
+                    result[i] = array1[j];
+                }
+            return result;
+        }
+
+        public static string Display(int[] array)
+        {
+            string str = array[0].ToString();
+            for (int i = 1; i < array.Length; ++i)
+            {
+                str += "," + array[i].ToString();
+            }
+            return str;
+        }
+    }
+}
